079da66727dc82fb70254c19541701186206087e
IgorCRH <125159948+IgorCRH@users.noreply.github.com>
2023-03-28 17:27:26 -0300

    Add files via upload

---
+++ 4 - Fatorial/fatorial.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class fatorial
+{
+    
+    public void naonulo (int num, int fat){
+    System.out.printf("O fatorial do numero eh %d",fat);
+    }
+    
+    public void nulo (){
+    System.out.println("O fatorial de 0! eh 1");
+    }
+    
+    public static void main(String[] args){
+    Scanner leitura = new Scanner (System.in);
+    fatorial objNaoNulo = new fatorial();
+    fatorial objNulo = new fatorial();
+    int num, fat = 1;
+    
+    System.out.println("Entre com o numero desejado");
+    num=leitura.nextInt();
+    
+    if (num == 0){
+    fat = 1;    
+    objNulo.nulo();
+    } else {
+    for (fat = 1; num > 1; num = num-1){
+    fat = fat*num;
+    }    
+    objNaoNulo.naonulo(num, fat);
+    }  
+}
+}
