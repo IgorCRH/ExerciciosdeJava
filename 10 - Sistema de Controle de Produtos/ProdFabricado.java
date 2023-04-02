e4959d26d48d433c0bbbe825182e2cb5325c4b07
IgorCRH <125159948+IgorCRH@users.noreply.github.com>
2023-04-01 23:19:41 -0300

    Update ProdFabricado.java

--- 10 - Sistema de Controle de Produtos/ProdFabricado.java
+++ 10 - Sistema de Controle de Produtos/ProdFabricado.java
@@ -1,13 +1,13 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 
-public class ProdFabricado implements IProdutoFabricado
+public class ProdFabricado implements IProdutoFabricado // Implementa a interface IProdutoFabricado
 {
 private String nome;
-protected ArrayList<lProduto> Ingrediente;
+protected ArrayList<lProduto> Ingrediente; // Criar Array para ser o vetor de strings com os ingredientes
 Scanner leitura = new Scanner (System.in);
 
-public ProdFabricado(String nome, ArrayList<lProduto> Ingrediente) {
+public ProdFabricado(String nome, ArrayList<lProduto> Ingrediente) { // Construtor da Classe ProdFabricado
 this.nome = nome;
 this.Ingrediente = Ingrediente;
 }
@@ -16,12 +16,12 @@ public String getNome() {
 return nome;
 }
 
-@Override
+@Override // Sobrepõe para delimitar a quantidade dos ingredientes
 public int getNumeroIngredientes() {
 return Ingrediente.size();
 }
 
-@Override
+@Override // Sobrepõe para delimitar os ingredientes
 public lProduto getIngrediente(int numero) {
 if (numero >= 0 && numero < Ingrediente.size()) {
 return Ingrediente.get(numero);
@@ -30,7 +30,7 @@ return null;
 }
 }
 
-@Override
+@Override // Sobrepõe para delimitar a soma do custo dos produtos fabricados
 public float getCusto() {
 float custo = 0;
 for (lProduto ingred : Ingrediente) {
@@ -39,7 +39,7 @@ custo += ingred.getCusto();
 return custo;
 }
 
-public void adicionarIngrediente(ProdComprados ingrediente) {
+public void adicionarIngrediente(ProdComprados ingrediente) { // Método para adicionar ingredientes
 Ingrediente.add(ingrediente);
 }
 }
