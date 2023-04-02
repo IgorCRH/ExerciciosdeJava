a11b73a6b62765243dd90b0ddb2d308cec9b29a0
IgorCRH <125159948+IgorCRH@users.noreply.github.com>
2023-04-01 23:21:27 -0300

    Update ProdComprados.java

--- 10 - Sistema de Controle de Produtos/ProdComprados.java
+++ 10 - Sistema de Controle de Produtos/ProdComprados.java
@@ -1,26 +1,26 @@
 import java.util.Scanner;
 
-public class ProdComprados implements lProduto
+public class ProdComprados implements lProduto // Implementa a Interface lProduto
 {
 protected String nome;
 private float custo;
 Scanner leitura = new Scanner (System.in);
 
-@Override
+@Override // Sobrepõe para delimitar o nome do produto. O método getNome, aonde estará a variável nome, é um dos campos da interface implementada.
 public String getNome(){
 System.out.println("Entre com nome do produto: \n");
 nome = leitura.next();
 return nome;
 }
 
-@Override
+@Override // Sobrepõe para delimitar o custo do produto. O método getCusto, aonde estará a variável custo, é um dos campos da interface implementada.
 public float getCusto(){
 System.out.println("Entre com o custo do produto: \n");
 custo = leitura.nextFloat();
 return custo;
 }
 
-public ProdComprados (String nome, float custo) {
+public ProdComprados (String nome, float custo) { // Construtor da Classe ProdComprados
 this.nome = nome;
 this.custo = custo;
 }
